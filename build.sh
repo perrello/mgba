4aea5d6691f1e54da6f13fa596332bacb9f40446
perrello <locomain12@gmail.com>
2026-01-28 16:52:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,31 @@ set -e
 CORE_NAME="mgba"
 BUILD_DIR="build-emscripten"
 OUTPUT_DIR="dist-wasm"
+HAVE_THREADS=0
+PROXY_TO_PTHREAD=0
+HAVE_RWEBAUDIO=1
 
 RETROARCH_DIR="./retroarch-linker"   # Add this yourself in your own repo
 CORE_BC="${CORE_NAME}_libretro_emscripten.bc"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --pthreads)
+      HAVE_THREADS=1
+      PROXY_TO_PTHREAD=1
+      HAVE_RWEBAUDIO=0
+      shift
+      ;;
+    -h|--help)
+      echo "Usage: $0 [--pthreads]"
+      exit 0
+      ;;
+    *)
+      echo "ERROR: unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
 
 echo "======== 1. Ensure RetroArch linker directory exists ========"
 if [ ! -d "$RETROARCH_DIR" ]; then
@@ -51,11 +73,14 @@ echo "======== 6. Build RetroArch WASM linker ========"
 cd "$RETROARCH_DIR"
 
 emmake make -f Makefile.emscripten LIBRETRO=${CORE_NAME} clean
-emmake make -f Makefile.emscripten LIBRETRO=${CORE_NAME} HAVE_AL=0 -j all VERBOSE=1
+emmake make -f Makefile.emscripten LIBRETRO=${CORE_NAME} HAVE_AL=0 HAVE_THREADS=${HAVE_THREADS} PROXY_TO_PTHREAD=${PROXY_TO_PTHREAD} HAVE_RWEBAUDIO=${HAVE_RWEBAUDIO} -j all VERBOSE=1
 
 echo "======== 7. Export output ========"
 cp ${CORE_NAME}_libretro.js   "../$OUTPUT_DIR/${CORE_NAME}.js"
 cp ${CORE_NAME}_libretro.wasm "../$OUTPUT_DIR/${CORE_NAME}.wasm"
+if [ -f "${CORE_NAME}_libretro.worker.js" ]; then
+  cp "${CORE_NAME}_libretro.worker.js" "../$OUTPUT_DIR/${CORE_NAME}.worker.js"
+fi
 
 cd ..
 
@@ -63,3 +88,6 @@ echo "======== DONE ========"
 echo "Built:"
 echo "  $OUTPUT_DIR/${CORE_NAME}.js"
 echo "  $OUTPUT_DIR/${CORE_NAME}.wasm"
+if [ -f "$OUTPUT_DIR/${CORE_NAME}.worker.js" ]; then
+  echo "  $OUTPUT_DIR/${CORE_NAME}.worker.js"
+fi
